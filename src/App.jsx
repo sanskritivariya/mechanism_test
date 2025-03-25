90d2dcbd25af3b94b6eadc82ff3c817c00a4b530
Sanskritivariya <sanskritivariya25@gmail.com>
2025-03-25 12:00:35 +0530

    practical

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+
+  const onhandleStart = () => {
+    const timer = setInterval(() => {
+      setFlag(!flag);
+      
+      setTime((prev) => {
+        if (prev < 100) {
+        
+          return prev+1;
+        }
+        if (prev >= 100) {
+          clearInterval(timer);
+          // setTime(0)
+          setFlag(true);
+        }
+        return prev;
+      // setFlag(true);
+      //   setTime((pre)=>{
+      //     pre+1
+        })
+      
+    }, time);
+  };
+
+  const handleinputChange = (e) => {
+    setFlag(false);
+    setTime(e.target.value);
+  };
+
+  console.log("after time set", time);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="body">
+        <div className="input-div">
+          <input
+            type="number"
+            value={time}
+            onChange={handleinputChange}
+            disabled={flag?true:false}
+          ></input>
+          <Button variant="primary" onClick={onhandleStart} disabled={flag?true:false} className="button">
+            Start
+          </Button>
+
+          <div>
+            <ProgressBar now={flag ? time : 0} label={`${flag ? time : 0}%`} />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
